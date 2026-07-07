f63a67d769bdb8d31ed23c4dbe3505f93cb7732d
olizero2 <olizero200@gmail.com>
2026-07-06 19:14:41 -0700

    Added some random input thing

--- StringManipulation.py
+++ StringManipulation.py
@@ -8,3 +8,8 @@ list = ["Diana ", "Mola ", "Aba "]
 newsentence = "no i ".join(list)
 
 print(newsentence)
+
+choice = input("Whats your name?")
+
+if choice.strip().lower().startswith("o"):
+    print("no i dont like people with O")
