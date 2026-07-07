062275e752ddb3623f5989ffd63207a72ff92ed4
olizero2 <olizero200@gmail.com>
2026-07-07 06:02:13 -0700

    Basic version of try / except

---
+++ tryexcept.py
@@ -0,0 +1,10 @@
+while True:
+
+    try:
+        num1 = int(input("Enter the first number: "))
+        num2 = int(input("Enter the second number: "))
+
+        print(f"{num1} + {num2} = {num1 + num2}")
+        break
+    except ValueError:
+        print("Invalid")
